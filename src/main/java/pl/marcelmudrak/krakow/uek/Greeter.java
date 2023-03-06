a87fe0df8a9114ef802b78ff9e963b16fbb2a397
Marcel-Mudrak <marcel-mudrak@wp.pl>
2023-03-06 12:49:43 +0100

    added greeter class

---
+++ src/main/java/pl/marcelmudrak/krakow/uek/Greeter.java
@@ -0,0 +1,27 @@
+package pl.marcelmudrak.krakow.uek;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Greeter {
+    public String greet(String name){
+        return "Hello " + name;
+    }
+
+    public static List<String> ladies(List<String> names){
+        var ladies = new ArrayList<String>();
+
+        for (String name : names) {
+            if (name.endsWith("a")) {
+                ladies.add(name);
+            }
+        }
+        return ladies;
+
+//        names.stream()
+//                .filter(name -> name.endsWith("a"))
+//                .forEach(greeter::greet);
+
+    }
+}
--- src/main/java/pl/marcelmudrak/krakow/uek/Main.java
+++ src/main/java/pl/marcelmudrak/krakow/uek/Main.java
@@ -1,8 +1,17 @@
 package pl.marcelmudrak.krakow.uek;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        var names = Arrays.asList("Jakub", "Michał", "Agnieszka", "Ola", "Kasia");
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.greet("Jakub")); // -> Hello Jakub
+
+        // Greet just ladies
+        System.out.println(Greeter.ladies(names));
+
     }
 }

571ef2b2cb109fef301140599d977b1f973445c5
Marcel-Mudrak <marcel-mudrak@wp.pl>
2023-03-20 12:07:37 +0100

    fractal number representations tests

---
+++ src/test/java/pl/marcelmudrak/creditcard/NumberRepresentationTest.java
@@ -0,0 +1,42 @@
+package pl.marcelmudrak.creditcard;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class NumberRepresentationTest {
+
+    @Test
+    void testDoubleRepresentation() {
+        double a = 0.02;
+        double b = 0.03;
+        double c = b - a;
+
+        System.out.println("Double 0.02 - 0.03:");
+        System.out.println(c);
+        System.out.println("Double 0.003 - 0.002:");
+        System.out.println(a - b);
+    }
+
+    @Test
+    void testFloatRepresentation() {
+        float a = 0.002f;
+        float b = 0.003f;
+        float c = b - a;
+
+        System.out.println("Float 0.002 - 0.003:");
+        System.out.println(c);
+        System.out.println("Float 0.003 - 0.002:");
+        System.out.println(a - b);
+    }
+
+    @Test
+    void testBigDecimalRepresentation() {
+        BigDecimal a = new BigDecimal("0.002");
+        BigDecimal b = new BigDecimal("0.003");
+        BigDecimal c = a.subtract(b);
+
+        System.out.println("Big Decimal 0.02 - 0.03:");
+        System.out.println(c);
+    }
+}
